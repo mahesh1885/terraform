e67dab4ae1ba3d98cf3c545d773799439b0e7e78
mahesh1885 <chitikalaumamahesh@gmail.com>
2024-09-21 18:57:04 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -7,10 +7,14 @@ variable "ami" {
 variable "type" {
   default = "t2.micro"
 }
+variable "number" {
+  default = "3"
+}
 resource "aws_instance" "test" {
     ami = var.ami
     instance_type = var.type
     key_name = "Mahesh1302"
+    count = var.number
     tags = {
       Name = "terraform"
     }
